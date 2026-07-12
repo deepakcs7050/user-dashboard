76ade076ffe70be506d23fbeab55c0795ea2b418
deepakcs7050 <dk705047@gmail.com>
2026-07-12 13:21:56 +0530

    add base in vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -5,6 +5,7 @@ const useMockApi = process.env.VITE_USE_MOCK_API !== 'false'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/user-dashboard",
   define: {
     __VITE_API_BASE_URL__: JSON.stringify(process.env.VITE_API_BASE_URL ?? 'http://localhost:5173/api'),
     __VITE_USE_MOCK_API__: useMockApi,
